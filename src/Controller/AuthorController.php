1166cf74e9e4f7c61531638a867acc2e768e6c69
oxyme <theobeulque281200@gmail.com>
2026-03-26 15:45:57 +0100

    ExerciceAuthor

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -9,21 +9,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Repository\BooksRepository;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\Books;
+use App\Repository\AuthorRepository;
+use App\Entity\Author;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-    
-
-final class BookController extends AbstractController
+final class AuthorController extends AbstractController
 {
-    #[Route('api/books', name: 'api_books', methods: ['GET'])]
-    public function getBookList(BooksRepository $booksRepository, SerializerInterface $serializer): JsonResponse
+     #[Route('api/authors', name: 'api_authors', methods: ['GET'])]
+    public function getAuthorList(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
     {
-        $bookList = $booksRepository->findAll();
-        $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
+        $authorList = $authorRepository->findAll();
+        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getBooks']);
 
-        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);   
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);   
     }
 
     // #[Route('api/books/{id}', name: 'detail_book', methods: ['GET'])]
@@ -39,11 +39,10 @@ final class BookController extends AbstractController
          
     // }
 
-    #[Route('api/books/{id}', name: 'detail_book', methods: ['GET'])]
-    public function getDetailBook(Books $book, SerializerInterface $serializer): JsonResponse
+    #[Route('api/authors/{id}', name: 'detail_author', methods: ['GET'])]
+    public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse
     {   
-        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
-        return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'application/json'], true);       
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getBooks']);
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, ['accept' => 'application/json'], true);       
     }
-    
-}    
+}
--- src/Entity/Books.php
+++ src/Entity/Books.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\BooksRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Author;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BooksRepository::class)]
 class Books
@@ -12,13 +14,20 @@ class Books
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+        #[Groups(["getBooks"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["getBooks"])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(["getBooks"])]
     private ?string $CoverText = null;
+
+    #[ORM\ManyToOne(inversedBy: 'Books')]
+    #[Groups(["getBooks"])]
+    private ?Author $author = null;
 
     public function getId(): ?int
     {
@@ -45,6 +54,18 @@ class Books
     public function setCoverText(?string $CoverText): static
     {
         $this->CoverText = $CoverText;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): static
+    {
+        $this->author = $author;
 
         return $this;
     }
